381dcd644b4c86ef7fb758cf9ed7fd5c03c77002
lynnetteeee <lynnette.oxh@gmail.com>
2024-03-08 16:05:44 +0800

    Update RemarkCommand and RemarkCommandParser, link to GUI

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -2,4 +2,5 @@ package seedu.address.commons.core;
 
 public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Message is in an invalid command format! \n%1$s";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
 }
